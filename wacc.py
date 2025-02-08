ebf8d92593e33dac72f9bcb6ff395070a785538d
Josué Magro <josuemagro16@gmail.com>
2025-02-08 01:13:42 -0300

    Add Interest Expense

--- wacc.py
+++ wacc.py
@@ -1,6 +1,6 @@
 import yfinance as yf
 
-stock = yf.Ticker("AAPL")
+stock = yf.Ticker("VBBR3.SA")
 
 risk_free_asset = 0.1325
 
@@ -15,3 +15,15 @@ def calculate_cost_of_equity(risk_free_asset, beta, market_return):
 cost_of_equity = calculate_cost_of_equity(risk_free_asset, beta, market_return)
 
 print(f"Cost of Equity: {cost_of_equity:.2f}%")
+
+
+def interest_expense():
+    income_statement = stock.financials
+
+    if "Interest Expense" in income_statement.index:
+        interest_expense_recent = income_statement.loc["Interest Expense"].iloc[0]
+        recent_year = income_statement.columns[0].strftime("%Y") 
+        return interest_expense_recent
+    else:
+        print("Interest Expense não encontrado na demonstração de resultados.")
+
