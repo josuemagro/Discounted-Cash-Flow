c64beed86e7937ceb17e38535c277260143d6c58
Josué Magro <josuemagro16@gmail.com>
2025-02-08 00:30:14 -0300

    Add the Function to Calculate the Cost of Equity

--- wacc.py
+++ wacc.py
@@ -8,3 +8,10 @@ market_return = 0.114
 
 beta = stock.info.get("beta")
 
+def calculate_cost_of_equity(risk_free_asset, beta, market_return):
+    ke = (risk_free_asset + beta * (market_return - risk_free_asset)) * 100
+    return ke
+
+cost_of_equity = calculate_cost_of_equity(risk_free_asset, beta, market_return)
+
+print(f"Cost of Equity: {cost_of_equity:.2f}%")
