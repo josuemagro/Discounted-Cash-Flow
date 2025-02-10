d1b7103fbd759c2ee334704e73cb9c929ce0f504
Josué Magro <josuemagro16@gmail.com>
2025-02-10 07:46:39 -0300

    Introduced functions to check for missing data before passing it to the user

--- functions.py
+++ functions.py
@@ -1,7 +1,9 @@
+import math
+
 def calculate_cost_of_equity(risk_free_asset, beta, market_return):
     ke = (risk_free_asset + beta * (market_return - risk_free_asset)) * 100
     return ke
-    
+
 def get_most_recent_value(data, metric_name):
     if metric_name in data.index:
         return data.loc[metric_name].iloc[0]
@@ -19,3 +21,11 @@ def effective_tax_rate(income_tax_expense, income_before_tax):
 def cost_of_debt_after_tax (cost_of_debt, tax_rate):
     calculate_cost_of_debt_after_tax = cost_of_debt * (1 - tax_rate / 100)
     return calculate_cost_of_debt_after_tax
+
+def check_and_display_variables(variables):
+    for name, value in variables.items():
+        if math.isnan(value)== False:
+            return True
+        else:
+            print(f"{name} not found.\n")
+            break
--- wacc.py
+++ wacc.py
@@ -1,7 +1,8 @@
 import yfinance as yf
 import functions
+import math
 
-stock = yf.Ticker("VBBR3.SA")
+stock = yf.Ticker("AAPL") #Edit This
 
 risk_free_asset = 0.1325 #Edit This
 
@@ -10,6 +11,7 @@ market_return = 0.114 #Edit This
 
 income_statement = stock.financials
 balance_sheet = stock.balance_sheet
+
 
 beta = stock.info.get("beta")
 
@@ -31,11 +33,42 @@ cost_of_debt_after_tax = functions.cost_of_debt_after_tax(cost_of_debt, tax_rate
 
 total_debt = functions.get_most_recent_value(balance_sheet, "Total Debt")
 
+market_cap = stock.info.get("marketCap")
 
-print(f"Cost of Equity: {cost_of_equity:.2f}%")
+sum_of_total_debt_and_market_cap = total_debt + market_cap
 
-print(f"Cost of Debt after Tax: {cost_of_debt_after_tax:.2f}%")
+total_debt_percent = total_debt / sum_of_total_debt_and_market_cap
 
-print(f"Total Debt: {total_debt}")
+market_cap_percent = market_cap / sum_of_total_debt_and_market_cap
+
+wacc = cost_of_debt * total_debt_percent + cost_of_equity * market_cap_percent
+
+variables_to_check = {
+    "Interest Expense": interest_expense,
+    "Total Debt": total_debt,
+    "Tax provision": income_tax_expense,
+    "Pretax Income": income_before_tax,
+    "Total Debt": total_debt,
+    "Market Cap": market_cap,
+}
+
+print(f"Ticker: {stock.info["symbol"]}\n")
+
+
+check_data = functions.check_and_display_variables(variables_to_check)
+
+if check_data == True:
+    print(f"Cost of Equity: {cost_of_equity:.2f}%")
+
+    print(f"Cost of Debt after Tax: {cost_of_debt_after_tax:.2f}%")
+
+    print(f"WACC: {wacc:.2f}%") 
+else:
+    print("Sorry, Some recent data might be missing from Yahoo Finance.")
+    print("You could manually change the value of the variable")
+
+
+
+
 
 
